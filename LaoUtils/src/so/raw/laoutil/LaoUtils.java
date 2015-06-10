d3262c3e2c88b0d91c6f55b070328cf0ad838ffb
miosec <miosec@gmail.com>
2015-06-10 22:27:45 +0800

    在LaoUtils中添加InputStream转String,以及在主线程和子线程中均可弹出Toast的静态方法.

--- LaoUtils/src/so/raw/laoutil/LaoUtils.java
+++ LaoUtils/src/so/raw/laoutil/LaoUtils.java
@@ -1,9 +1,13 @@
 package so.raw.laoutil;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 
 import android.app.Activity;
 import android.view.View;
+import android.widget.Toast;
 
 public class LaoUtils {
 
@@ -53,6 +57,51 @@ public class LaoUtils {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		}
+	}
+	/**
+	 * 将InputStream转换为String
+	 * @param InputStream is
+	 * @return String 
+	 * @throws UnsupportedEncodingException
+	 */
+	public static String converStream2String(InputStream is) throws UnsupportedEncodingException{
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		byte[] buffer = new byte[512];
+		int len = 0;
+		
+		try {
+			while((len = is.read(buffer))!=-1){
+				baos.write(buffer,0,len);
+			}
+			is.close();
+			return new String(baos.toByteArray());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return new String(baos.toByteArray(),"gb2312");
+	}
+	/**
+	 * 无论子线程还是子线程均可显示Toast
+	 * @param activity 上下文对象
+	 * @param msg  消息内容
+	 * @param duration 时长 可以选择 0或1
+	 */
+	public static void ShowToast(final Activity activity,final String msg,final int duration){
+		//判断是否主线程
+		if("main0".equals(Thread.currentThread().getName())){
+			//当前线程为主线程
+			Toast.makeText(activity, msg, duration).show();
+		}else{
+			activity.runOnUiThread(new Runnable() {
+				
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					Toast.makeText(activity, msg, duration).show();
+				}
+			});
 		}
 	}
 }
